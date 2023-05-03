62f072138db0f0843f1df9ef9d9c2b1fff6b8d71
stefanselic <s.stefan22@hotmail.com>
2023-05-03 14:33:19 +0200

    Update code and other files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
-import logo from './logo.svg';
+import { useState } from 'react';
 
 export default function App() {
+  const [hex, setHex] = useState('#ffffff');
+  const randomizedHex = () => {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+    setHex(randomColor);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        backgroundColor: hex,
+        minHeight: '33.34vh',
+        overflow: 'hidden',
+      }}
+    >
+      <div>
+        <br />
+        <p id="hex-code">Generated Color: {hex}</p>
+        <button onClick={randomizedHex}>Generate</button>
+      </div>
     </div>
   );
 }
